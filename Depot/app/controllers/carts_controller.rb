e6c61947309fc601cf9478f47c8256106a23c975
Varunram <vrg2009@ymail.com>
2016-08-19 18:25:03 +0530

    Shopping Cart Ready

--- Depot/app/controllers/carts_controller.rb
+++ Depot/app/controllers/carts_controller.rb
@@ -1,6 +1,11 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
+  def invalid_cart
+      logger.error "Attempt To Access Invalid Cart #{params[:id]}"
+      redirect_to store_url, notice: "Invalid Cart Entry"
+  end
   # GET /carts
   # GET /carts.json
   def index
@@ -54,9 +59,10 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart_id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url }
+      format.html { redirect_to store_url, notice: "Your Cart is currently Empty"}
       format.json { head :no_content }
     end
   end

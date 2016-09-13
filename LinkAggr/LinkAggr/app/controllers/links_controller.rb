621a972b6e80299276e9259b843ad3b0127ef158
Varunram <vrg2009@ymail.com>
2016-09-13 15:19:29 +0530

    Added Authorization

--- LinkAggr/LinkAggr/app/controllers/links_controller.rb
+++ LinkAggr/LinkAggr/app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, except: [:index, :show]
+
   # GET /links
   # GET /links.json
   def index
@@ -14,7 +14,7 @@ class LinksController < ApplicationController
 
   # GET /links/new
   def new
-    @link = current_user.links.build 
+    @link = Link.new
   end
 
   # GET /links/1/edit
@@ -24,7 +24,7 @@ class LinksController < ApplicationController
   # POST /links
   # POST /links.json
   def create
-    @link = current_user.links.build(link_params)
+    @link = Link.new(link_params)
 
     respond_to do |format|
       if @link.save
